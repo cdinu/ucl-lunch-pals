ecadaa46c8c2dd23d7b8cd8f13602c2991293671
Cristian Dinu <c.dinu@ucl.ac.uk>
2024-03-06 11:48:13 +0000

    add hardcoded data for people and eateries

---
+++ data/eateries.ts
@@ -0,0 +1,32 @@
+const eateries = [
+    {
+        id: 0,
+        name: 'Arret A Manger',
+        tags: ['Desserts', 'Salads', 'Fast Food'],
+        image: 'https://www.arretamanger.com/wp-content/uploads/2021/07/Arret-a-Manger-Logo-1.png',
+        type: 'Casual',
+        zone: 0,
+    },
+    {
+        id: 1,
+        name: 'Falafel Things',
+        tags: ['Middle Eastern', 'Halal', 'Fast Food'],
+        type: 'Fast Food'
+        zone: 0,
+    },
+    {
+        id: 2,
+        name: 'LEON',
+        tags: ['Healthy', 'Salads', 'Fast Food'],
+        zone: 0,
+        type: 'Casual',
+        image: 'https://leon.co/assets/images/logos/leon-black-square.svg',
+    },
+    {
+        id: 3,
+        name: 'Bidborough Ground Floor Kitchen',
+        tags: ['Bring Your Own'],
+        type: 'BYO',
+        zone: 0,
+    },
+];
---
+++ data/people.ts
@@ -0,0 +1,28 @@
+const people = [
+    {
+        id: 1,
+        name: 'John Doe',
+        department: 'ISD',
+        title: 'Software Developer',']
+    },
+    {
+        id: 2,
+        name: 'Jane Doe',
+        department: 'ISD',
+        title: 'Software Developer'
+    },
+    {
+        id: 3,
+        name: 'Jim Doe',
+        department: 'ISD',
+        title: 'Software Developer'
+    },
+    {
+        id: 4,
+        name: 'Jill Doe',
+        department: 'ISD',
+        title: 'Software Developer'
+    }
+];
+
+export default people;
